d87e3d67ec09bf38769fb322a76d8000e7d71798
Daniël Groeneweg <daniel.groeneweg03@gmail.com>
2024-11-22 00:42:52 +0100

    Wire Task Progress
    Bug:
    Wire does not scale correctly, pivot also doesn't seem to be at the right even though it is set to right.

--- Assets/Scripts/Wire.cs
+++ Assets/Scripts/Wire.cs
@@ -1,40 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Wire : MonoBehaviour
 {
-    public SpriteRenderer wireEnd;
+    public Image wireEnd;
     Vector3 startPoint;
-    void Start()
+    private bool isDragging = false;
+
+    private void Start()
     {
-        startPoint = transform.parent.position;
+        startPoint = wireEnd.rectTransform.position;
     }
-
-    /*
-    private void OnMouseDrag()
-    {
-        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        newPosition.z = 0;
-
-        transform.position = newPosition;
-
-        Vector3  direction = newPosition - startPoint;
-        transform.right = direction * transform.lossyScale.x;
-
-        float dist = Vector2.Distance(startPoint, newPosition);
-        wireEnd.size = new Vector2(dist, wireEnd.size.y);
-    }
-    */
 
     private void Update()
     {
-        if (transform.parent.name == "Wire Red")
-        {
-            Debug.Log(Input.mousePosition);
+        if (!isDragging && Input.GetMouseButtonDown(0) && IsPointerOverUIElement()) isDragging = true;
 
-            Debug.Log("Wire: " + transform.position);
+        if (isDragging)
+        {
+            StretchWire();
         }
+    }
+
+
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    public bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.transform.parent.parent == transform.parent)
+                return true;
+        }
+        return false;
+    }
+
+    //Gets all event system raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
+    private void StretchWire()
+    {
+        // Reset the position
+        transform.position = startPoint;
+
+        // Get the mouse input
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = 0;
+
+        // Set the rotation to look at the mouse
+        wireEnd.rectTransform.LookAt(mouse);
+        var rotation = wireEnd.rectTransform.rotation;
+        rotation = new Quaternion(0, 0, -rotation.x, 1);
+        wireEnd.rectTransform.rotation = rotation;
+
+        // Get the direction from the wire start to the mouse
+        Vector3 direction = mouse - startPoint;
+
+        // Set the position to the mouse
+        Vector3 newPosition = mouse;
+        transform.position = newPosition;
+
+        // Resize the wire to so it filles from the start position to the mouse position
+        float dist = Vector2.Distance(startPoint, newPosition);
+        wireEnd.rectTransform.sizeDelta = new Vector2(100 + dist, wireEnd.rectTransform.sizeDelta.y);
     }
 }
